f40fa5bafb8f67a994246508b2bc369cafe43d0a
Rohith11 <rohithbuddala@gmail.com>
2026-08-02 12:13:35 +0530

    Time: 0 ms (100%), Space: 43.1 MB (48.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,39 @@
 class Solution {
-   public boolean stoneGame(int[] piles) {
-    return true;
-   }
+    public boolean stoneGame(int[] piles) {
+        long[] players = new long[2];
+        players = getWinner(piles, 0, piles.length - 1);
+        if(players[0] < players[1]) { return false; }
+        return true;
+    }
+    
+
+    public long[] getWinner(int[] stones, int start, int last) {
+        int alice = 0, bob = 0;
+        long[] playersScore = new long[2];
+        if (start < last) {
+            if(stones[start] >= stones[last]) {
+                alice = stones[start];
+                start += 1;
+            } else {
+                alice = stones[last];
+                last -= 1;
+            }
+
+            if(stones[start] <= stones[last]) {
+                bob = stones[start];
+                start += 1;
+            } else {
+                bob = stones[last];
+                last -= 1;
+            }
+            playersScore = getWinner(stones, start, last);
+            playersScore[0] += alice;
+            playersScore[1] += bob;
+            return playersScore;
+        }
+
+        playersScore[0] += alice;
+        playersScore[1] += bob;
+        return playersScore;
+    }
 }
