580ed2389c0491de8f75f43b011f95750998876f
Rohith11 <rohithbuddala@gmail.com>
2026-08-22 17:58:28 +0530

    Time: 0 ms (100%), Space: 44.6 MB (70.37%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int breakValue = -1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if(nums[i] < nums[i+1]) {
-                breakValue = i;
-                break;
-            }
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
         }
 
-        if(breakValue == -1) arrRev(nums, 0);
-        else {
-
-            for(int i=nums.length-1; i>=breakValue; i--) {
-           
-                if(nums[i] > nums[breakValue]) {
-                    int temp = nums[i];
-                    nums[i] = nums[breakValue];
-                    nums[breakValue] = temp;
-                    break;
-                }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
             }
-
-            arrRev(nums, breakValue+1);
+            swap(nums, i, j);
         }
+
+        arrRev(nums, i+1, nums.length-1);
+
     }
 
-    public void arrRev(int[] nums, int left) {
-        int right = nums.length-1;
-        while (left < right) {
-           int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
+    private void arrRev(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
             left++;
             right--;
         }
