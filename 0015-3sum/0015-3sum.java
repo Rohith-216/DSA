8e77f1a006f0d9a711e994c65371a328369e9c62
Rohith11 <rohithbuddala@gmail.com>
2026-09-13 10:33:57 +0530

    Time: 32 ms (76.26%), Space: 59.5 MB (23.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -20,7 +20,7 @@ class Solution {
                     k--;
 
                     while(j<k && nums[j] == nums[j-1]) j++;
-                    while(j > k && nums[k] == nums[k+1]) k++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
